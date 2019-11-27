9da1582a5c1a7ddcbba5d522f3fc700e89ddfdc3
rizqyep <rizqyepr@gmail.com>
2019-11-28 00:55:56 +0700

    Update subcheck.py

--- subcheck.py
+++ subcheck.py
@@ -1,19 +1,20 @@
 def substringcheck(string,sub):
-    start = 0
-    count = 0
-    condition = True
+    start = 0 #initialize start index for searching with 0 
+    count = 0 #initialize counter with 0
+    condition = True #initialize condition with True to make the searching going on as long as it still find substring inside string
     while condition :
-        check = string.find(sub,start)
-        if check == -1 :
+        check = string.find(sub,start) """using str.find() method to find in which index it should start seeking,with a parameters of sub 
+        and start values """
+        if check == -1 : #return false if the substring not found in string
             condition = False
         else :
-            count +=1
-            start = check+1
+            count +=1 #adding 1 to count everytime the program encounter a substring searched in a string
+            start = check+1 #start value will be changed based on which index the str.find() method found the substring
     return(count)
 
 if __name__ == '__main__':
-    string = input("Enter string : ").strip()
-    sub = input("Enter substring to search for : ").strip()
+    string = input("Enter string : ").strip() #prompt input for string 
+    sub = input("Enter substring to search for : ").strip() #prompt input for substring that will be searched and counted
     
-    count = substringcheck(string, sub)
-    print(count)
+    substringsum = substringcheck(string, sub)#call the substringcheck function and save the return value into substringsum variable
+    print(substringsum)  
